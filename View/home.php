7ac9db0a28379aa464afb992578c03e19a499194
Matheus <matheusedulimapinheiro@gmail.com>
2024-10-18 12:56:31 -0300

    Corrigindo a integridade dos dados dos usuários

--- View/home.php
+++ View/home.php
@@ -1,9 +1,8 @@
-<!-- home.php -->
 <?php
 session_start(); // Inicia a sessão
 
 // Verifica se o usuário está logado
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
     header("Location: ../View/auth_login.php"); // Redireciona para a página de login se não estiver logado
     exit;
 }
@@ -14,8 +13,14 @@ require_once("../Module/conexao.php");
 // Aqui você pode incluir a lógica para buscar os dados do usuário
 $c = new Conexao();
 $userData = $c->getUserDataById($_SESSION['user_id']); // Busca os dados do usuário
-?>
 
+// Verifica se os dados do usuário foram encontrados
+if ($userData === null) {
+    // Redireciona para a página de login se o usuário não for encontrado
+    header("Location: ../View/auth_login.php");
+    exit;
+}
+?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
